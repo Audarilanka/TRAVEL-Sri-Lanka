212e1eb5c3defd75e5198c79171627b504bc4060
Audarilanka <dissanayakelanka96@gmail.com>
2022-08-12 10:53:41 +0530

    feat(project)travelApp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Pages/Home";
+// import MainSection from "./components/HeroSection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        {/* <Home /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/" exact />
+          <Route />
+          {} */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
