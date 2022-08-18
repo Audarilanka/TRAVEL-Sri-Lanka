595cb2fe3dc72081cdf3828c8a47bc372e24f25f
Audarilanka <dissanayakelanka96@gmail.com>
2022-08-18 11:13:26 +0530

    feat(travelapp)project2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Pages/Home";
+import Services from "./components/Pages/Services";
+import Products from "./components/Pages/Product";
+import SignUp from "./components/Pages/SignUp";
+import Footer from "./components/Footer";
 // import MainSection from "./components/HeroSection";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -13,10 +17,15 @@ function App() {
         {/* <Home /> */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
           {/* <Route path="/" exact />
           <Route />
           {} */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
